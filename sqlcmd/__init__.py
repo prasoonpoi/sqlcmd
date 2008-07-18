2da1caf95f4957c30a979eb023e7752b499a329e
Brian Clapper <bmc@clapper.org>
2008-07-18 15:20:55 +0000

    bumped version number

--- sqlcmd/__init__.py
+++ sqlcmd/__init__.py
@@ -79,7 +79,7 @@ from enum import Enum
 # ---------------------------------------------------------------------------
 
 # Info about the module
-__version__   = '0.2.1'
+__version__   = '0.2.2'
 __author__    = 'Brian Clapper'
 __email__     = 'bmc@clapper.org'
 __url__       = 'http://www.clapper.org/software/python/sqlcmd/'
