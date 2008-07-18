18ded7894095d1394a48e856437af45360ebe229
Brian Clapper <bmc@clapper.org>
2008-07-18 17:13:18 +0000

    Documented history module.

--- sqlcmd/history.py
+++ sqlcmd/history.py
@@ -10,11 +10,11 @@ Stick to the methods in the History class.
 $Id$
 """
 
-__docformat__ = 'restructuredtext'
-
 # ---------------------------------------------------------------------------
 # Imports
 # ---------------------------------------------------------------------------
+
+from __future__ import with_statement
 
 import re
 import sys
@@ -22,12 +22,14 @@ import logging
 import copy
 
 from grizzled.decorators import abstract
+from grizzled.exception import ExceptionWithMessage
 
 # ---------------------------------------------------------------------------
 # Exports
 # ---------------------------------------------------------------------------
 
-__all__ = ['getHistory', 'History', 'DEFAULT_MAXLENGTH']
+__all__ = ['getHistory', 'History', 'DEFAULT_MAXLENGTH', 'HistoryError']
+__docformat__ = 'restructuredtext'
 
 # ---------------------------------------------------------------------------
 # Constants
@@ -46,9 +48,9 @@ _have_pyreadline = False
 try:
     import readline
     _have_readline = True
-    
+
     # Is it pyreadline? If so, it's not quite the same.
-    
+
     try:
         _have_pyreadline = readline.rl.__module__.startswith('pyreadline.')
     except AttributeError:
@@ -63,12 +65,12 @@ except ImportError:
 def get_history(verbose=True):
     """
     Factory method to create an appropriate History object.
-    
+
     :Parameters:
         verbose : bool
             ``True`` to display a message on standard output about what
             history management mechanism is being used.
-            
+
     :rtype: ``History``
     :return: the ``History`` object
     """
@@ -96,16 +98,43 @@ def get_history(verbose=True):
 # Classes
 # ---------------------------------------------------------------------------
 
-class History(object):
+class HistoryError(ExceptionWithMessage):
+    """
+    Thrown to indicate history errors, when another exception won't do.
+    """
+    pass
 
+class History(object):
+    """
+    Base class for history implementations.  All concrete history
+    implementations must extend this class.
+    """
     def __init__(self):
         self.set_max_length(DEFAULT_MAXLENGTH)
 
-    def show(self):
+    def show(self, out=sys.stdout):
+        """
+        Dump the history to a file-like object (defaulting to standard output).
+
+        :Parameters:
+            out : file
+                Where to dump the history.
+        """
         for i in range(1, self.total + 1):
-            print '%4d: %s' % (i, self.get_item(i))
+            print >> out, '%4d: %s' % (i, self.get_item(i))
 
     def get_last_matching_item(self, command_name):
+        """
+        Get the most recently entered item that matches ``command_name``
+        at the beginning.
+
+        :Parameters:
+            command_name : str
+                The string to match against the commands in the history
+
+        :rtype: str
+        :return: the matching string, or ``None``
+        """
         result = None
         for i in range(self.get_total(), 0, -1):
             s = self.get_item(i)
@@ -117,23 +146,67 @@ class History(object):
         return result
 
     def get_last_item(self):
+        """
+        Get the most recent item in the history.
+
+        :rtype: str
+        :return: The most recent command, or ``None``
+        """
         return self.get_item(self.get_total() - 1)
 
     def get_item(self, index):
+        """
+        Get an item from the history.
+
+        :Parameters:
+            index : int
+                0-based index of the item to get. The larger the index
+                value, the more recent the entry
+
+        :rtype: str
+        :return: the item at that index
+
+        :raise IndexError: Index out of range
+        """
         return None
 
     def set_completer_delims(self, s):
+        """
+        Set the completer delimiters--the characters that delimit tokens
+        that are eligible for completion.
+
+        :Parameters:
+            s : str
+                The delimiters
+        """
         pass
-    
+
     def get_completer_delims(self):
+        """
+        Get the completer delimiters--the characters that delimit tokens
+        that are eligible for completion.
+
+        :rtype: str
+        :return: the delimiters
+        """
         return ''
 
     @property
     def total(self):
-        """The total size of the history"""
+        """
+        The total number number of commands in the history. Identical to
+        calling ``get_total()``.
+        """
         return self.get_total()
 
     def get_total(self):
+        """
+        Get the total number number of commands in the history. Identical to
+        the ``total`` property.
+
+        :rtype: int
+        :return: the number of commands in the history
+        """
         return 0
 
     def __set_max_length(self, n):
@@ -147,25 +220,70 @@ class History(object):
 
     @abstract
     def get_max_length(self):
+        """
+        Get the maximum length of the history. This isn't the maximum number
+        of entries in the in-memory history buffer; instead, it's the maximum
+        number of entries that will be saved to the history file. Subclasses
+        *must* provide an implementation of this method.
+
+        :rtype: int
+        :return: the maximum saved size of the history
+        """
         pass
 
     @abstract
     def set_max_length(self, n):
+        """
+        Set the maximum length of the history. This isn't the maximum number
+        of entries in the in-memory history buffer; instead, it's the maximum
+        number of entries that will be saved to the history file.  Subclasses
+        *must* provide an implementation of this method.
+
+        :Parameters:
+            n : int
+                the maximum saved size of the history
+        """
         pass
 
     @abstract
     def add_item(self, line):
+        """
+        Add (append) a line to the history buffer. Subclasses *must* provide
+        an implementation of this method.
+
+        :Parameters:
+            line : str
+                the command to append to the history
+        """
         pass
 
     @abstract
     def remove_item(self, i):
+        """
+        Remove a line from the history buffer. Subclasses *must* provide an
+        implementation of this method.
+
+        :Parameters:
+            i : int
+                the 0-based index of the item to be removed
+        """
         pass
 
     @abstract
     def clear_history(self):
+        """
+        Clear the history buffer. Subclasses *must* provide an
+        implementation of this method.
+        """
         pass
 
     def get_history_list(self):
+        """
+        Get a copy of the history buffer.
+
+        :rtype: list
+        :return: a list of commands from the history
+        """
         result = []
         for i in range(1, self.total + 1):
             result += [self.get_item(i)]
@@ -173,7 +291,20 @@ class History(object):
         return result
 
     def remove_matches(self, regexp_string):
-        pat = re.compile(regexp_string)
+        """
+        Remove all history items that match a regular expression.
+
+        :Parameters:
+            regexp_string : str
+                the uncompiled regular expression to match
+
+        :raise HistoryError: bad regular expression
+        """
+        try:
+            pat = re.compile(regexp_string)
+        except:
+            raise HistoryError(str(sys.exc_info[1]))
+
         buf = []
 
         for i in range(1, self.total + 1):
@@ -184,6 +315,17 @@ class History(object):
         self.replace_history(buf)
 
     def cut_back_to(self, index):
+        """
+        Cut the history back to the specified index, removing all entries
+        more recent than that index.
+
+        :Parameters:
+            index : int
+                the index of the command that should become the last command
+                in the history
+
+        :raise IndexError: index out of range
+        """
         if (index > 0) and (index <= self.total):
             buf = []
             for i in range(1, index):
@@ -191,25 +333,51 @@ class History(object):
 
             self.replace_history(buf)
 
-    def replace_history(self, buf):
+    def replace_history(self, commands):
+        """
+        Replace the entire contents of the history with another set of values
+
+        :Parameters:
+            commands : list
+                List of strings to put in the history after clearing it of any
+                existing entries
+        """
         self.clear_history()
-        for s in buf:
-            self.add_item(s, force=True)
+        for command in commands:
+            self.add_item(command, force=True)
 
-    def save_history_file(self, file):
-        log.debug('Writing history file "%s"' % file)
-        f = open(file, "w")
-        for i in range(1, self.total + 1):
-            f.write(self.get_item(i) + '\n')
-        f.close()
+    def save_history_file(self, path):
+        """
+        Save the history to a file. The file is overwritten with the contents
+        of the history buffer.
 
-    def load_history_file(self, file):
-        log.debug('Loading history file "%s"' % file)
-        f = open(file)
-        buf = []
-        for line in f:
-            buf += [line.strip()]
-        f.close()
+        :Parameters:
+            path : str
+                Path to the history file to receive the output.
+
+        :raise IOError: Unable to open file
+        """
+        log.debug('Writing history file "%s"' % path)
+        with open(path, 'w') as f:
+            for i in range(1, self.total + 1):
+                f.write(self.get_item(i) + '\n')
+
+    def load_history_file(self, path):
+        """
+        Load the history buffer with the contents of a file, completely
+        replacing the in-memory history with the file's contents.
+
+        :Parameters:
+            path : str
+                Path to the history file to read
+
+        :raise IOError: Unable to open file
+        """
+        log.debug('Loading history file "%s"' % path)
+        with open(path, 'r') as f:
+            buf = []
+            for line in f:
+                buf += [line.strip()]
 
         max = self.get_max_length()
         if len(buf) > max:
@@ -231,7 +399,7 @@ class ReadlineHistory(History):
 
     def set_completer_delims(self, s):
         readline.set_completer_delims(s)
-    
+
     def get_completer_delims(self,):
         return readline.get_completer_delims()
 
@@ -284,7 +452,7 @@ class PyReadlineHistory(ReadlineHistory):
 
     def set_completer_delims(self, s):
         readline.set_completer_delims(s)
-    
+
     def get_completer_delims(self):
         return readline.get_completer_delims()
 
